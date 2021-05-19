132af28d0325a545516c0480180bd6057778b78d
Célia Cornec <celia.cornec@student.isae-supaero.fr>
2021-05-19 11:19:29 +0200

    Succès dans la création des boutons d'abonnement

--- projetIndividuel/communitymanager/urls.py
+++ projetIndividuel/communitymanager/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('communautes', views.communautes, name='Liste des communautés'),
-    path('abonnement/<int:communaute_id>/<int:choix>', views.modifabonnement, name='Modification abonnement'),
+    path('communautes', views.communautes, name='Liste des communautes'),
+    path('communautes/<int:communaute_id>/<int:choix>', views.communautes, name='Modification abonnement'),
     path('communaute/<int:communaute_id>', views.communaute, name='Affichage des posts'),
 ]
--- projetIndividuel/communitymanager/views.py
+++ projetIndividuel/communitymanager/views.py
@@ -6,14 +6,25 @@ from .models import Communaute, Post
 # Create your views here.
 
 @login_required() #Pensez à mettre ça partout !
-def communautes(request):
+def communautes(request, communaute_id=0, choix=2):
     communautes = Communaute.objects.all()
     user = request.user
+
+    if choix == 0:
+        communaute_modif = Communaute.objects.get(id=communaute_id)
+        communaute_modif.abonnes.remove(request.user)
+        #communaute_modif.abonnement = False
+    elif choix == 1:
+        communaute_modif = Communaute.objects.get(id=communaute_id)
+        communaute_modif.abonnes.add(request.user)
+        #communaute_modif.abonnement = True
+
     for communaute in communautes:
         if user in communaute.abonnes.all():
             communaute.abonnement = True
         else:
             communaute.abonnement = False
+
     return render(request, 'communitymanager/communautes.html', locals())
 
 @login_required()
@@ -21,12 +32,15 @@ def modifabonnement(request, communaute_id, choix):
     communaute = Communaute.objects.get(id=communaute_id)
     if choix == 0:
         communaute.abonnes.remove(request.user)
+        communaute.abonnement = False
     else:
         communaute.abonnes.add(request.user)
+        communaute.abonnement = True
+    communautes = Communaute.objects.all()
     return render(request, 'communitymanager/communautes.html', locals())
 
 @login_required()
 def communaute(request, communaute_id):
     communaute = Communaute.objects.get(id=communaute_id)
-    posts = Post.objects.all(communaute=communaute)
+    posts = Post.objects.filter(communaute=communaute)
     return render(request, 'communitymanager/communaute.html', locals())
